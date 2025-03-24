841a89a09687cc90e004b3619c21305b84ef267f
onegood07 <one_good07@naver.com>
2025-03-24 18:50:03 +0900

    feat: Lotto 클래스 메서드 추가 - 중복 확인, 로또 번호 랜덤 부여

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,12 +13,14 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  // 함수 - 로또 번호 6개인지 확인
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
+  // 함수 - 로또 번호에 중복이 있는지 확인
   #validateDuplicate() {
     const set = new Set(numbers);
     if (set.length != 6) {
@@ -26,7 +28,13 @@ class Lotto {
     }
   }
 
-  getLottoNumbers() {
+  // 함수 - 1~45 범위의 로또 번호 6개 랜덤 부여
+  pickRandomLottoNumbers() {
+    return (this.#numbers = Random.pickUniqueNumbersInRange(1, 45, 6));
+  }
+
+  // 함수 - 오름차순 정렬된 로또 번호 6개 배열 호출
+  getSortedLottoNumbers() {
     return this.#numbers.sort((a, b) => a - b);
   }
 }
